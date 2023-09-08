8ec6e2fcab0a69f5ce6f3e2e2758f7d970fc746a
rivercity310 <h970126@gmail.com>
2023-09-08 20:20:35 +0900

    #1 - SecurityConfig

---
+++ src/main/kotlin/com/example/server/config/SecurityConfig.kt
@@ -0,0 +1,22 @@
+package com.example.server.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.server.SecurityWebFilterChain
+
+@Configuration
+@EnableWebFluxSecurity
+class SecurityConfig {
+    @Bean
+    fun securityFilterChain(http: ServerHttpSecurity) : SecurityWebFilterChain {
+        return http
+            .csrf { it.disable() }
+            .httpBasic { it.disable() }
+            .formLogin { it.disable() }
+            .logout { it.disable() }
+            .authorizeExchange { it.pathMatchers("/api/v1/**").permitAll() }
+            .build()
+    }
+}
