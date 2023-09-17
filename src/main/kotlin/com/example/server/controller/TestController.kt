b32289620616046a475a22bb8d3fbfffc53a75b7
rivercity310 <h970126@gmail.com>
2023-09-17 18:29:08 +0900

    #1 - CI/CD Setting

--- src/main/kotlin/com/example/server/controller/TestController.kt
+++ src/main/kotlin/com/example/server/controller/TestController.kt
@@ -4,8 +4,6 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.reactive.function.server.ServerResponse
-import reactor.core.publisher.Mono
 
 @RestController
 class TestController {
