7840f9430b7e99f95d7e06e0c269ea4f2571cd2f
rivercity310 <h970126@gmail.com>
2023-09-18 11:39:23 +0900

    #9 - OAuth2 소셜로그인 연동

--- src/main/kotlin/com/example/server/repository/UserRepository.kt
+++ src/main/kotlin/com/example/server/repository/UserRepository.kt
@@ -5,4 +5,6 @@ import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface UserRepository : CoroutineCrudRepository<User, Int>
+interface UserRepository : CoroutineCrudRepository<User, Int> {
+    suspend fun existsByEmail(email: String) : Boolean
+}
